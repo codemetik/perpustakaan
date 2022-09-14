144bdf12d2bbc8c9bb99033476616a9b2206c9d8
codemetik <irfanovaiyalkharish03@gmail.com>
2022-09-14 12:33:19 +0700

    rubah koneksi

--- config.php
+++ config.php
@@ -1,16 +1,15 @@
 <?php
-
-
+$host = "localhost";
+$user = "smkg6671";
+$pass = "smksapra2123";
+$db = "smkg6671_perpustakaan";
 
 // $host = "localhost";
-// $user = "smkg6671";
-// $pass = "smksapra2123";
+// $user = "root";
+// $pass = "";
 // $db = "smkg6671_perpustakaan";
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "smkg6671_perpustakaan";
+
 
 $config = mysqli_connect($host, $user, $pass, $db) or die ('Could not connect : '. mysqli_connect_error());
 
