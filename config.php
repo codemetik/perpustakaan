0fdb322890120dd2e2e4738e6ed3ffa2b92ae7b0
codemetik <irfanovaiyalkharish03@gmail.com>
2022-09-14 09:49:59 +0700

    memperbaiki error koneksi database

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
 
-// $host = "localhost";
-// $user = "smkg6671";
-// $pass = "smksapra2123";
-// $db = "smkg6671_perpustakaan";
+
 
 $host = "localhost";
-$user = "root";
-$pass = "";
+$user = "smkg6671";
+$pass = "smksapra2123";
 $db = "smkg6671_perpustakaan";
+
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $db = "smkg6671_perpustakaan";
 
 $config = mysqli_connect($host, $user, $pass, $db) or die ('Could not connect : '. mysqli_connect_error());
 
